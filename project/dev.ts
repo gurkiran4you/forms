a57d5d6c0f5c9b3e985a71fc85b8443e3e73ad5c
Gurkiran Singh Randhawa <gukiece@gmail.com>
2024-07-21 17:01:54 -0600

    mino

--- project/dev.ts
+++ project/dev.ts
@@ -13,7 +13,8 @@ import { runBenchmarks } from "https://deno.land/x/pretty_benching@v0.3.3/deps.t
 import { bench } from "https://deno.land/std@0.91.0/testing/bench.ts";
 import { initiatePsebPb } from "./fetchData/pb/pseb.ts";
 import * as path from "jsr:@std/path";
-import { existsSync } from "$std/fs/exists.ts";
+import { exists, existsSync } from "$std/fs/exists.ts";
+import logger from "./logs/log.ts";
 
 const isInDeployment = Deno.env.get('DENO_DEPLOYMENT_ID') != null;
 
@@ -25,17 +26,20 @@ const __dirname = path.dirname(path.fromFileUrl(import.meta.url));
 
 // create store files folders
 const storeFilesFolder = 'storeFiles';
-const storePathFound = existsSync(path.join(__dirname, storeFilesFolder));
+const storePathFound = await exists(path.join(__dirname, storeFilesFolder));
 if (!storePathFound) {
     // create 
-    Deno.mkdirSync(path.join(__dirname, storeFilesFolder));
-    Deno.mkdirSync(path.join(__dirname, storeFilesFolder, 'pb'))
-    Deno.mkdirSync(path.join(__dirname, storeFilesFolder, 'pb', 'pseb'))
-    Deno.mkdirSync(path.join(__dirname, storeFilesFolder, 'pb', 'pspcl'))
-    Deno.mkdirSync(path.join(__dirname, storeFilesFolder, 'pb', 'general'))
-    Deno.mkdirSync(path.join(__dirname, storeFilesFolder, 'pb', 'ceo'))
+    await Deno.mkdir(path.join(__dirname, storeFilesFolder));
+    const perms = await Deno.permissions.request({ name: 'write', path: path.join(__dirname, storeFilesFolder)});
+    if (perms.state === "granted") {
+        console.log('granted');
+    }
+    await Deno.mkdir(path.join(__dirname, storeFilesFolder, 'pb'))
+    await Deno.mkdir(path.join(__dirname, storeFilesFolder, 'pb', 'pseb'))
+    await Deno.mkdir(path.join(__dirname, storeFilesFolder, 'pb', 'pspcl'))
+    await Deno.mkdir(path.join(__dirname, storeFilesFolder, 'pb', 'general'))
+    await Deno.mkdir(path.join(__dirname, storeFilesFolder, 'pb', 'ceo'))
 }
-
 // cron job 
 //new Cron("*/10 * * * * *", () => {
     // await initiateGeneralPb();
--- project/logs/log.ts
+++ project/logs/log.ts
@@ -1,8 +1,12 @@
 import * as path from "jsr:@std/path";
 import Logger from "https://deno.land/x/logger@v1.1.6/logger.ts";
 const __dirname = path.dirname(path.fromFileUrl(import.meta.url));
-const storeDir = path.join(__dirname);
+const storeDir = path.join(__dirname, 'logs');
 const logger = new Logger();
+const perms = await Deno.permissions.request({ name: 'write', path: storeDir});
+if (perms.state === "granted") {
+    console.log('granted');
+}
 await logger.initFileLogger(storeDir);
 logger.disableConsole();
 
