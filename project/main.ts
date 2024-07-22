f3fe1171d29f4df55897784f9b8fb29a5c1da877
Gurkiran Singh Randhawa <gukiece@gmail.com>
2024-07-21 18:34:11 -0600

    minor

--- project/main.ts
+++ project/main.ts
@@ -4,15 +4,14 @@
 /// <reference lib="dom.asynciterable" />
 /// <reference lib="deno.ns" />
 import "$std/dotenv/load.ts";
+import mongoose from "npm:mongoose@^6.7";
+
+const uri = Deno.env.get('MONGO_URI') as string;
+
+await mongoose.connect(uri);
 
 import { start } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
 import config from "./fresh.config.ts";
-
-import mongoose from "npm:mongoose@^6.7";
-
-
-const uri = Deno.env.get('MONGO_URI') as string;
-await mongoose.connect(uri);
 
 await start(manifest, config);
--- project/pre.ts
+++ project/pre.ts
@@ -3,9 +3,16 @@ import { initiateCeoPb } from "./fetchData/pb/ceo.ts";
 import { initiateGeneralPb } from "./fetchData/pb/general.ts";
 import { initiatePsebPb } from "./fetchData/pb/pseb.ts";
 import { initiatePspclPb } from "./fetchData/pb/pspcl.ts";
+import mongoose from "npm:mongoose@^6.7";
+
+const uri = Deno.env.get('MONGO_URI') as string;
+
+await mongoose.connect(uri);
 
 await initiateGeneralPb();
 await initiateCategoriesPb();
 await initiatePspclPb();
 await initiateCeoPb();
 await initiatePsebPb();
+
+Deno.exit();
