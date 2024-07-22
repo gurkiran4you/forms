666117973a23b0a8ce2387dd5c4377995fdb659f
Gurkiran Singh Randhawa <gukiece@gmail.com>
2024-07-21 18:44:37 -0600

    minor

--- project/pre.ts
+++ project/pre.ts
@@ -6,7 +6,6 @@ import { initiatePspclPb } from "./fetchData/pb/pspcl.ts";
 import mongoose from "npm:mongoose@^6.7";
 
 const uri = Deno.env.get('MONGO_URI') as string;
-
 await mongoose.connect(uri);
 
 await initiateGeneralPb();
