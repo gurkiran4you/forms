eaa9844c248c19d35acf8c2911b901c0aeb61371
Gurkiran Singh Randhawa <gukiece@gmail.com>
2024-07-21 18:24:44 -0600

    minor

--- project/main.ts
+++ project/main.ts
@@ -9,4 +9,10 @@ import { start } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
 import config from "./fresh.config.ts";
 
+import mongoose from "npm:mongoose@^6.7";
+
+
+const uri = Deno.env.get('MONGO_URI') as string;
+await mongoose.connect(uri);
+
 await start(manifest, config);
--- project/pre.ts
+++ project/pre.ts
@@ -3,12 +3,6 @@ import { initiateCeoPb } from "./fetchData/pb/ceo.ts";
 import { initiateGeneralPb } from "./fetchData/pb/general.ts";
 import { initiatePsebPb } from "./fetchData/pb/pseb.ts";
 import { initiatePspclPb } from "./fetchData/pb/pspcl.ts";
-import mongoose from "npm:mongoose@^6.7";
-
-
-const uri = Deno.env.get('MONGO_URI') as string;
-
-await mongoose.connect(uri);
 
 await initiateGeneralPb();
 await initiateCategoriesPb();
